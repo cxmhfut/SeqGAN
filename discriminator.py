e4e018d55edcda933ba38a691e7e4144b97d298b
cxmhfut <912750125@qq.com>
2018-07-06 16:34:46 +0800

    update

--- discriminator.py
+++ discriminator.py
@@ -92,6 +92,9 @@ class Discriminator(object):
             with tf.device('/cpu:0'), tf.name_scope('embedding'):
                 self.W = tf.Variable(tf.random_uniform([self.vocab_size, self.embedding_size], -1.0, 1.0),
                                      name='W')
+                # input_x.shape = (batch_size,sequence_length)
+                # embedded_chars.shape = (batch_size,sequence_length,embedding_size)
+                # embedded_chars.shape = (batch_size,sequence_length,embedding_size,1)
                 self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
                 self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
